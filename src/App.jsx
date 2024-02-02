4ec8e163b3fb3d1bafa6d241cc10731542453072
Naronglit2015 <nongmnakoy1123@gmail.com>
2024-02-02 18:22:49 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,18 @@
-import React, { useState } from "react";
-import RegisterForm from "./layout/RegisterForm";
-import LoginForm from "./layout/LoginForm";
+import useAuth from "./hooks/useAuth";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
-  const [isLoginForm, setIsLoginForm] = useState(true);
+  const {loading} = useAuth()
 
-  const toggleForm = () => {
-    setIsLoginForm(!isLoginForm);
-  };
+  if(loading) {
+    return (
+      <p className="text-4xl text-primary">Loading..</p>
+    )
+  }
 
   return (
-    <div data-theme="" className="min-h-screen flex flex-col items-center justify-center">
-      <h1 style={{ width: '60%' }} className="text-5xl text-white text-center border py-5 m-6 font-bold rounded-md shadow-md bg-gradient-to-r from-pink-500 via-pink-600 to-pink-700">
-  Hello world!
-</h1>
-
-
-
-
-      <input
-        type="checkbox"
-        value="synthwave"
-        className="toggle theme-controller"
-      />
-
-      <hr />
-
-      {isLoginForm ? <LoginForm /> : <RegisterForm />}
-
-      <button onClick={toggleForm} className="my-4">
-        {isLoginForm ? "Switch to Register Form" : "Switch to Login Form"}
-      </button>
+    <div className="min-h-screen">
+      <AppRouter />
     </div>
   );
 }
